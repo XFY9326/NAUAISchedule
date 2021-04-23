09f4d8a9e5e94527a7bd850c50600c4e42996368
1069665464@qq.com <1069665464@qq.com>
2021-04-23 20:20:29 +0800

    更新文件 scheduleHtmlProvider.js

--- scheduleHtmlProvider.js
+++ scheduleHtmlProvider.js
@@ -1,11 +1,14 @@
 function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
-    // 获取内部Frame
-    let innerFrame = dom.getElementById("FrameContent");
-    let frameSrc = innerFrame.getAttribute('src');
-    // 判断当前内部Frame是否是课程表页面
-    if (frameSrc === "MyCourseScheduleTable.aspx" || frameSrc === "MyCourseScheduleTableNext.aspx") {
-        // 获取课程表Table
-        return innerFrame.contentDocument.getElementById("content").innerHTML;
+    try{
+        // 获取内部Frame
+        let innerFrame = dom.getElementById("FrameContent");
+        let frameSrc = innerFrame.getAttribute('src');
+        // 判断当前内部Frame是否是课程表页面
+        if (frameSrc === "MyCourseScheduleTable.aspx" || frameSrc === "MyCourseScheduleTableNext.aspx") {
+            // 获取课程表Table
+            return innerFrame.contentDocument.getElementById("content").innerHTML;
+        }
+    } catch(err) {
+        throw Error("Error ScheduleTable Page!");
     }
-    throw Error("Error ScheduleTable Page!");
 }
